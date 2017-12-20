1dd527ca794ff92e1188092b179b844ea040c8e4
rafaelguerreroclemente <rafa.guerrero@gmail.com>
2017-12-20 18:00:36 +0100

    Polymer npm

--- STLFrontend/src/main/js/main.js
+++ STLFrontend/src/main/js/main.js
@@ -2,11 +2,4 @@
  * Created by rafaelguerreroclemente on 15/7/17.
  */
 
-import FormPost from 'components/FormPost';
-
-function main() {
-    let formPost = document.querySelector(".stl-form-post");
-    !!formPost && (new FormPost(formPost));
-}
-
-main();
+//import '../html/custom-element.html'
