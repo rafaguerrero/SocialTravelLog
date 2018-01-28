72dc9cbf30e53385277c89b974afae6ff25b81b6
aldana <lap1804@gmail.com>
2018-01-28 14:27:10 +0100

    Canvio de las jsp por html y controllers modificados para registrar un autor nuevo y llegar desde la home al login

--- STLFrontend/src/main/views/author/createsuccess.jsp
+++
@@ -1,9 +0,0 @@
-<%@taglib prefix="interface" tagdir="/WEB-INF/tags/interface" %>
-
-
-<interface:body className="author author--page">
-    <h1>Welcome!!</h1>
-    <h2>${author.name}</h2>
-
-
-</interface:body>
--- STLWeb/src/main/java/com/stl/web/services/Register.java
+++ STLWeb/src/main/java/com/stl/web/services/Register.java
@@ -1,9 +1,7 @@
 package com.stl.web.services;
 
 import org.springframework.dao.DuplicateKeyException;
-import com.stl.db.ArticleDB;
 import com.stl.db.AuthorDB;
-import com.stl.entity.Article;
 import com.stl.entity.Author;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,17 +10,14 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 @Controller
-@RequestMapping(value = "/author")
-public class AuthorController {
+@RequestMapping(value = "/register")
+
+public class Register {
 
     @Autowired
     private AuthorDB authorDB;
-
-    @Autowired
-    private ArticleDB articleDB;
 
     @RequestMapping(value = {"", "/"}, method = RequestMethod.POST)
     public ModelAndView createAuthor(@RequestBody @ModelAttribute("author") Author author,
@@ -30,54 +25,31 @@ public class AuthorController {
                                      HttpServletRequest request,
                                      HttpServletResponse response) {
         ModelAndView mav = new ModelAndView();
+        mav.setViewName("/register/index");
 
         if (author == null) {
             mav.addObject("status", "genericError");
-            mav.setViewName("/author/create");
 
         } else {
             try {
                 authorDB.save(author);
 
-                mav.addObject("status", "success");
-                mav.setViewName("/author/createsuccess");
-                response.setStatus(HttpServletResponse.SC_CREATED);
-
+                response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+                response.setHeader("Location", "/author/" + author.getUserId());
             } catch (DuplicateKeyException e) {
                 mav.addObject("status", "authorAlreadyExists");
-                mav.setViewName("/author/create");
             } catch (Exception e) {
                 mav.addObject("status", "genericError");
-                mav.setViewName("/author/create");
             }
         }
 
         return mav;
     }
 
+
     @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
     public ModelAndView createAuthorForm() {
-        return new ModelAndView("/author/create", "author", new Author());
+        return new ModelAndView("/register/index", "author", new Author());
     }
 
-    @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
-    public ModelAndView getAuthorPage(@PathVariable String userId,
-                                    HttpServletRequest request,
-                                    HttpServletResponse response) {
-        ModelAndView mav = new ModelAndView();
-
-        Author author = authorDB.getByUserId(userId);
-
-        if(author != null) {
-            List<Article> article = articleDB.getByAuthor(author);
-
-            mav.addObject("author", author);
-            mav.addObject("articles", article);
-            mav.setViewName("/author/page");
-        } else {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        }
-
-        return mav;
-    }
 }
