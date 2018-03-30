aab253cad002c189591781b54400e078b1e7d391
rafaelguerreroclemente <rafa.guerrero@gmail.com>
2018-03-30 13:19:56 +0200

    CustomTags structure

--- STLTags/src/main/java/com/stl/tags/TravelerDataTag.java
+++ STLTags/src/main/java/com/stl/tags/TravelerDataTag.java
@@ -1,18 +1,16 @@
 package com.stl.tags;
 
 import com.stl.db.TravelerDB;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.stl.entity.Traveler;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
+import javax.servlet.ServletRequest;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 import java.io.IOException;
 
 public class TravelerDataTag extends SimpleTagSupport {
-
-    @Autowired
-    private TravelerDB travelerDB;
-
     private String username;
 
     public String getUsername() {
@@ -25,8 +23,15 @@ public class TravelerDataTag extends SimpleTagSupport {
 
     @Override
     public void doTag() throws JspException, IOException {
-        getJspContext().setAttribute("traveler", travelerDB.getByUsername(username), PageContext.PAGE_SCOPE);
+        Traveler traveler = getTravelerDB().getByUsername(username);
+
+        getJspContext().setAttribute("traveler", traveler, PageContext.PAGE_SCOPE);
         super.doTag();
         getJspContext().removeAttribute("traveler", PageContext.PAGE_SCOPE);
+    }
+
+    private TravelerDB getTravelerDB() {
+        ServletRequest request = ((PageContext) getJspContext()).getRequest();
+        return (TravelerDB) RequestContextUtils.getWebApplicationContext(request).getBean("travelerDB");
     }
 }
